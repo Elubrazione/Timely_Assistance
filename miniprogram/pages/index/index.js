3d726b8c9a5d4f7390e2db843efe017751cebbd6
jelly <1355532189@qq.com>
2022-03-08 14:41:11 +0800

    add openid function

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,4 +1,5 @@
 // pages/index/index.js
+const app=getApp()
 Page({
   data: {
       userInfo: {},
@@ -15,6 +16,18 @@ Page({
   getUserProfile(e) {
       // 使用wx.getUserProfile获取用户信息 开发者每次通过该接口获取用户个人信息均需用户确认
       // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+      wx.cloud.callFunction({
+          name: 'login',
+          data: {},
+          success: res => {
+            app.globalData.openid = res.result.openid
+            wx.setStorageSync("myOpenId", res.result.openid);
+            wx.setStorage({
+                key: "User_openid",
+                data: app.globalData.openid
+              })
+          }
+      })
       wx.getUserProfile({
           // 声明获取用户个人信息后的用途，后续会展示在弹窗中 谨慎填写
           desc: '您的授权将用于完善用户初始资料',
