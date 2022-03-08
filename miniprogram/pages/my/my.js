5200ce34f582bc656ea4b12434ed85ab25ed81c5
lxy-meteor-shower <u202015438@hust.edu.cn>
2022-03-08 14:50:48 +0800

    somemy

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -1,27 +1,18 @@
-/*
-const db = wx.cloud.database({ })
-const _ = db.command
-*/
+
+// const db = wx.cloud.database({ })
+// const _ = db.command
+
 Page({
   data: {
     replyNumber: 0,
     toupNumber: 0,
     chatNumber: 0,
     hiddenmodalput: true,
-    Username:'',
+    Username: '',
     Join_Time:'',
     User_head_url:'',
     U_id:'',
     Oldname:''
-
-  },
-  //点击按钮
-  modalinput: function () {
-    this.setData({
-      Oldname: this.data.Username,
-      hiddenmodalput: !this.data.hiddenmodalput
-    })
-    console.log(this.data.Oldname)
   },
   //取消按钮
   cancel: function () {
@@ -58,33 +49,45 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    var that = this
-    wx.hideTabBarRedDot({
-      index: 1,
-    })
-    let Nowtime=Date.now();
-    db.collection('Assistant_User').where({
-      _openid: wx.getStorageSync("myOpenId"),
-    }).get({
-      success: res => {
-       // console.log(res);
-        if (86400000>(Nowtime - res.data[0].Creat_user_Time))
-        {
-          Nowtime=1;
-        }
-        else{
-          Nowtime=parseInt((Nowtime - res.data[0].Creat_user_Time) / 86400000)+1
-        }
-        console.log(Nowtime)
+    let that=this
+    //获取头像和名字
+    wx.getStorage({
+      key: 'userInfo',
+      success(res) {
+        console.log(res.data) //userinfo
         that.setData({
-          Username:res.data[0].Username,
-          User_head_url:res.data[0].User_head_url,
-          Last_to_Reply: res.data[0].Last_to_Reply,
-          Last_toup_Time: res.data[0].Last_toup_Time,
-          U_id:res.data[0]._id,
-          Join_Time: Nowtime
+          Username:res.data.nickName,
+          User_head_url:res.data.avatarUrl
         })
       }
     })
+
+    // wx.hideTabBarRedDot({
+    //   index: 1,
+    // })
+    // let Nowtime=Date.now();
+    // db.collection('Assistant_User').where({
+    //   _openid: wx.getStorageSync("myOpenId"),
+    // }).get({
+    //   success: res => {
+    //    // console.log(res);
+    //     if (86400000>(Nowtime - res.data[0].Creat_user_Time))
+    //     {
+    //       Nowtime=1;
+    //     }
+    //     else{
+    //       Nowtime=parseInt((Nowtime - res.data[0].Creat_user_Time) / 86400000)+1
+    //     }
+    //     console.log(Nowtime)
+    //     that.setData({
+    //       Username:res.data[0].Username,
+    //       User_head_url:res.data[0].User_head_url,
+    //       Last_to_Reply: res.data[0].Last_to_Reply,
+    //       Last_toup_Time: res.data[0].Last_toup_Time,
+    //       U_id:res.data[0]._id,
+    //       Join_Time: Nowtime
+    //     })
+    //   }
+    // })
   }
 })
